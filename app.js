d28e5a6f1daf972fb61bbc3973e08ca5ecbb9546
Eric Brooks <ericbrookspro@gmail.com>
2020-08-30 18:43:39 -0500

    accept user input

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const chalk = require('chalk')
 
 const getNotes = require('./notes')
 
-const msg = getNotes()
+const command = process.argv[2]
 
-console.log(msg)
-console.log(chalk.green.bold.inverse('Success!'))
+console.log(process.argv)
 
-// nodemon installed
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
